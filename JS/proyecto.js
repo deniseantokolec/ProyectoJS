fe97a8f936a24c2a8b4d4a8196f4f3f8b723c9a1
deniseantokolec <desarrollosweb.antokolec@gmail.com>
2022-03-09 15:53:23 -0300

    DOM 1

--- JS/proyecto.js
+++ JS/proyecto.js
@@ -1,5 +1,16 @@
 console.log("Este es el comienzo del proyecto final");
 
+//Aca empieza la seccion nosotros
+
+let nosotros = document.getElementById("nosotros")
+let parrafo1 = document.getElementById("nosotrostexto")
+parrafo1.innerText = "Net Web es una empresa dedicada al desarrollo web fundada por Denise Antokolec. Inicio su carrera en el mundo de la programación luego de recibirse de Licenciada en Administración de empresas, buscando una salida laboral que le permita desafiar la imaginación y el pensamiento en el día a día. Por eso realizó en Code House los cursos de Desarrollo Web y Javascript para poder brindar a sus clientes lo que buscan en base a sus necesidades y las de los usuarios de sus páginas web, considerando sus comnocimientos empresariales teniendo asi la posibilidad de aportar valor."
+
+console.log(parrafo1.innerText);
+
+//Aca termina la seccion nosotros
+
+
 //Acá empieza la seccion presupuesto
 
 //1. pedir periodo máximo de entrega
@@ -16,59 +27,76 @@ let valorHora = 1500
 let valorSeo = 50
 let valorDominio = 199
 let diasyhoras= 40
-let entrega = parseInt(prompt ("Escribir el tiempo máximo para la entrega en semanas"))
+let entrega = parseInt(document.getElementById("semmax"))
 
-function semanasmaximas (n){
-    return (m) => m<=n
-}
-semanasmaximas(5) (1)
-let cincosemanas = semanasmaximas (5)
-
-
-
-if (entrega>= cincosemanas(5)){
-    function cantidadhoras() {
-        let horas = entrega*diasyhoras
-        return horas
+function calcular () {
+    if (entrega>= 5){
+        function cantidadhoras() {
+            let horas = entrega*diasyhoras
+            return horas
         }
-}else {
-    alert ("El valor ingresado no es correcto")
+    }else {
+        alert ("El valor ingresado no es correcto")
+            
+    }
+    function valorhora(valorHora,cantidadhoras) {
+        let horasvalue = valorHora*cantidadhoras
+        return horasvalue
+    }
+
+    function testing(valorhora) {
+        let testeo = valorhora*0.9
+        return testeo
+    }    
+    const sumar = (a,b,c,d) => a+b+c+d
+
+    let total = sumar (valorhora(),valorDominio,testing(),valorSeo)
+    console.log(total);
+ 
+    return total
 }
 
+function calcular2 () {
+    if (entrega>= 5){
+        function cantidadhoras() {
+            let horas = entrega*diasyhoras
+            return horas
+            }
+    }else {
+        alert ("El valor ingresado no es correcto")
+    }
+    function valorhora(a,b) {
+        return a*b
+    }
+    function valordominio() {
+        if (document.getElementById("opciones3")=document.getElementById("opciones3"))
+        alert ("El valor de dominio por mes es:" + "$199")
+    }
+    function testing(valorhora,cantidadhoras) {
+        let testeo = valorhora*cantidadhoras
+        return testeo
+    }    
+    const sumar = (a,b,c,d) => a+b+c+d
+    let total2 = sumar (valorhora,cantidadhoras(),valorDominio,testing())
+
+    function alert (valortotal2) {
+        let valortotal2 = alert ("El valor total presupuestado es: " + "$" + total2)
+        return valortotal2
+    }
 
 
-function valorhora(a,b) {
-    return a*b
-}
 
-function valordominio() {
-    alert ("El valor de dominio por mes es:" + "$199")
     
-
-
 }
 
-function testing(a,b) {
-    let testeo = a*b
-    return testeo
-     
-}
-function seo () {
-    alert ("El valor de SEO es: " + "$50")
-    
-}
-
-valorhora (valorHora,cantidadhoras())
-valordominio ()
-testing(valorhora(valorHora,cantidadhoras()),0.9)
-seo ()
 
 
-const sumar = (a,b,c,d) => a+b+c+d
 
-let total = sumar (valorhora(valorHora,cantidadhoras()),valorDominio,testing(valorhora(valorHora,cantidadhoras()),0.9),valorSeo)
-console.log(total);
-let valortotal = alert ("El valor total presupuestado es: " + "$" + total)
+
+
+
+
+
 
 //Acá termmina la seccion presupuesto
 
