b64530a3dc30416ced68dcd26368b73ffa5b056c
deniseantokolec <desarrollosweb.antokolec@gmail.com>
2022-03-14 12:50:58 -0300

    Eventos y Dom parte 2

--- JS/proyecto.js
+++ JS/proyecto.js
@@ -11,6 +11,86 @@ console.log(parrafo1.innerText);
 //Aca termina la seccion nosotros
 
 
+
+
+//Acá empieza la seccion presupuesto
+
+console.log("Este es el comienzo del proyecto final-presupuesto");
+
+//1. pedir periodo máximo de entrega
+//2. calcular el precio de armar web con el valor ingresado + constante valor hora
+//3. dar el precio estimado de dominio
+//4. sumar precio de dominio
+//5. dar precio de testing
+//6. sumar el precio de testing a todo lo anterior
+//7. dar precio de SEO
+//8. precio final
+
+
+let valorHora = 1500
+let valorSeo = 50
+let valorDominio = 199
+let diasyhoras= 40
+let semanas = document.getElementById("semmax")
+let producto = document.getElementById("opciones")
+let calculo = document.getElementById("resultado")
+const boton = document.getElementById("calcular")
+boton.addEventListener("click",presupuesto)
+
+function presupuesto () {
+    if (producto == [1]){
+        calculo.innerHTML = valorHora*diasyhoras*semanas
+    }
+    else if (producto == [2]){
+        calculo.innerHTML = valorHora*diasyhoras*semanas+valorDominio+((valorHora*diasyhoras*semanas)*0.9)
+    }
+    else if (producto == [3]){
+        calculo.innerHTML = valorHora*diasyhoras*semanas+valorDominio+((valorHora*diasyhoras*semanas)*0.9)+valorSeo
+    }else{
+        alert("Los valores que ingreso no son los corresctos, por favor vuelva a ingresar para porder brindarle el resultado.")
+    }
+}
+
+
+
+//Acá termmina la seccion presupuesto
+
+
+
+//Acá emmpieza la seccion de contacto
+
+
+
+let nombre = document.getElementById("nombrec")
+let surname = document.getElementById("apellidoc")
+let reason = document.getElementById("descripcion1")
+let description = document.getElementById("descripcion")
+let email = document.getElementById("email")
+let telefono = document.getElementById("telefonoc")
+let codpost = document.getElementById("codigopostalc")
+let ciudad = document.getElementById ("ciudadc")
+
+const enviar = document.getElementById("enviar")
+enviar.addEventListener("submit",contacto)
+
+function contacto (e) {
+    console.log("Este es el comienzo del proyecto final-contacto");
+    e.preventDefault()
+    console.log("Se activo el Prevemnt Default");
+}
+
+
+formulario.addEventListener('formdata',(e) => {
+    
+    const formulario = [nombre,surname,email,telefono,codpost,ciudad,reason,description]
+    const datos = formulario.innerHTML.value
+    datos.innerText = e
+    console.log(e);
+})    
+
+
+//Acá termina la seccion contacto
+
 //Acá empieza la seecioón Faqs
 
 let Faqs = document.getElementById("faqs")
